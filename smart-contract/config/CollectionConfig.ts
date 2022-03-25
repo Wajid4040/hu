38da5f8db4a4cef950af74e5a87edf221d1ef774
Wajid4040 <95139058+Wajid4040@users.noreply.github.com>
2022-03-26 01:43:49 +0500

    Add files via upload

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import { ethereumTestnet, ethereumMainnet } from '../lib/Networks';
+import { openSea } from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: ethereumTestnet,
+  mainnet: ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'YourNftToken',
+  tokenName: 'My NFT Token',
+  tokenSymbol: 'MNT',
+  hiddenMetadataUri: 'ipfs://QmSNcfBrQ1rb8vuqvsNKiw6J7iywcauvRXhUFU3seqXDrh/hidden.json',
+  maxSupply: 10000,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: '0x31bD07E4CcFA77563aA645c4bb8840e195d62646',
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: openSea,
+  whitelistAddresses: whitelistAddresses,
+};
+
+export default CollectionConfig;
